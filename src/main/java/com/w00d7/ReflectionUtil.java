60ed021215c8672bdd8cd2a2749ec8a644e97f38
moses <46579721+glocation87@users.noreply.github.com>
2024-08-19 22:21:19 -0400

    Optimization | Caching methods & fields

--- src/main/java/com/w00d7/ReflectionUtil.java
+++ src/main/java/com/w00d7/ReflectionUtil.java
@@ -6,10 +6,19 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ReflectionUtil
 {
+    private static final Map<Class<?>, Map<String, Field>> fieldCache = new ConcurrentHashMap<>();
+    private static final Map<Class<?>, Map<String, Method>> methodCache = new ConcurrentHashMap<>();
+    private static final Logger logger = Logger.getLogger(ReflectionUtil.class.getName());
 
     /**
      * Returns the value of the field from the given instance/object
@@ -60,42 +69,10 @@ public class ReflectionUtil
 
     public static Object invokeMethod(Object object, String methodName, Class<?>[] parameterTypes, Object... args) 
         throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-            Method method = object.getClass().getDeclaredMethod(methodName, parameterTypes);
+            Method method = getCachedMethod(object.getClass(), methodName, parameterTypes);
             method.setAccessible(true);
             Object[] convertedArgs = convertToWrapperTypes(parameterTypes, args);
             return method.invoke(object, convertedArgs);
-    }
-
-     /**
-     * Gets the value of a static field from the given class.
-     *
-     * @param class_    the class from which to get the static field
-     * @param fieldName the name of the static field
-     * @return the value of the static field
-     * @throws NoSuchFieldException
-     * @throws IllegalAccessException
-     */
-    public static Object getStaticField(Class<?> class_, String fieldName)
-            throws NoSuchFieldException, IllegalAccessException {
-        Field field = class_.getDeclaredField(fieldName);
-        field.setAccessible(true);
-        return field.get(null);
-    }
-
-    /**
-     * Sets the value of a static field in the given class.
-     *
-     * @param class_    the class in which to set the static field
-     * @param fieldName the name of the static field
-     * @param value     the value to set
-     * @throws NoSuchFieldException
-     * @throws IllegalAccessException
-     */
-    public static void setStaticField(Class<?> class_, String fieldName, Object value)
-            throws NoSuchFieldException, IllegalAccessException {
-        Field field = class_.getDeclaredField(fieldName);
-        field.setAccessible(true);
-        field.set(null, value);
     }
 
     /**
@@ -112,10 +89,84 @@ public class ReflectionUtil
      */
     public static Object invokeStaticMethod(Class<?> class_, String methodName, Class<?>[] parameterTypes, Object... args)
             throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        Method method = class_.getDeclaredMethod(methodName, parameterTypes);
+        Method method = getCachedMethod(class_, methodName, parameterTypes);
         method.setAccessible(true);
         Object[] convertedArgs = convertToWrapperTypes(parameterTypes, args);
         return method.invoke(null, convertedArgs);
+    }
+
+    /**
+     * Used to retrieve/cache methods within hashmap
+     * 
+     * @param class_
+     * @param methodName
+     * @param parameterTypes
+     * @return
+     * @throws NoSuchMethodException
+     */
+    private static Method getCachedMethod(Class<?> class_, String methodName, Class<?>[] parameterTypes)
+        throws NoSuchMethodException {
+        Map<String, Method> methods = methodCache.computeIfAbsent(class_, k -> new HashMap<>());
+        String methodKey = methodName + Arrays.toString(parameterTypes);
+        return methods.computeIfAbsent(methodKey, key -> {
+            try {
+                return class_.getDeclaredMethod(methodName, parameterTypes);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+
+     /**
+     * Gets the value of a static field from the given class.
+     *
+     * @param class_    the class from which to get the static field
+     * @param fieldName the name of the static field
+     * @return the value of the static field
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    public static Object getStaticField(Class<?> class_, String fieldName)
+            throws NoSuchFieldException, IllegalAccessException {
+        Field field = getCachedField(class_, fieldName);
+        field.setAccessible(true);
+        return field.get(null);
+    }
+
+    /**
+     * Sets the value of a static field in the given class.
+     *
+     * @param class_    the class in which to set the static field
+     * @param fieldName the name of the static field
+     * @param value     the value to set
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    public static void setStaticField(Class<?> class_, String fieldName, Object value)
+            throws NoSuchFieldException, IllegalAccessException {
+        Field field = getCachedField(class_, fieldName);
+        field.setAccessible(true);
+        field.set(null, value);
+    }
+
+    /**
+     * Used to retrieve/cache fields within hashmap
+     * 
+     * @param class_
+     * @param fieldName
+     * @return
+     * @throws NoSuchFieldException
+     */
+    private static Field getCachedField(Class<?> class_, String fieldName) throws NoSuchFieldException {
+        Map<String, Field> fields = fieldCache.computeIfAbsent(class_, k -> new HashMap<>());
+        return fields.computeIfAbsent(fieldName, name -> {
+            try {
+                return class_.getDeclaredField(name);
+            } catch (NoSuchFieldException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     /**
@@ -155,7 +206,7 @@ public class ReflectionUtil
                 return Optional.of(Class.forName(className, true, classLoader));
             }
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Class not found: " + className, e);
             return Optional.empty();
         }
     }
@@ -171,15 +222,18 @@ public class ReflectionUtil
     public static Optional<Method> findMethod(Class<?> class_, String methodName, Object... args) {
         Method[] methods = class_.getDeclaredMethods();
         for (Method method : methods) {
-            if (method.getName().equals(methodName) && method.getParameterTypes().length == args.length) {
+            if (method.getName().equals(methodName) && Arrays.equals(method.getParameterTypes(), getParameterTypes(args))) {
                 return Optional.of(method);
             }
         }
         return Optional.empty();
     }
 
+    private static Class<?>[] getParameterTypes(Object[] args) {
+        return Arrays.stream(args).map(Object::getClass).toArray(Class<?>[]::new);
+    }
     /**
-     * Function to convert primitive types to a
+     * Convert primitives to type safe wrapped classes
      * 
      * @param parameterTypes
      * @param args
@@ -214,6 +268,25 @@ public class ReflectionUtil
         if (type == float.class) return Float.valueOf((float) value);
         if (type == double.class) return Double.valueOf((double) value);
         throw new IllegalArgumentException("Unsupported primitive type: " + type);
+    }
+
+    /**
+     * Unwrap primitive types
+     * 
+     * @param type
+     * @param value
+     * @return
+     */
+    private static Object unwrapPrimitive(Class<?> type, Object value) {
+        if (type == boolean.class) return ((Boolean) value).booleanValue();
+        if (type == byte.class) return ((Byte) value).byteValue();
+        if (type == char.class) return ((Character) value).charValue();
+        if (type == short.class) return ((Short) value).shortValue();
+        if (type == int.class) return ((Integer) value).intValue();
+        if (type == long.class) return ((Long) value).longValue();
+        if (type == float.class) return ((Float) value).floatValue();
+        if (type == double.class) return ((Double) value).doubleValue();
+        return value;
     }
 }   
 
